303720862cc0d3fc2e5911b0a13686770c2c290a
Kai Patel <00kaipatel@gmail.com>
2023-06-01 12:53:15 +0100

    feat: initial server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", routing::get(|| async { "Hello world!" }));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
