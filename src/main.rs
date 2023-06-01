c48020b1cf48652e04a0e7db22885545da808fba
Kai Patel <00kaipatel@gmail.com>
2023-06-01 13:00:27 +0100

    chore: refactor helloworld

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use axum::{Router, routing};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use axum::{routing, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", routing::get(|| async { "Hello world!" }));
+    let app = Router::new().route("/", routing::get(handler));
+    let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 3000);
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&address)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Hello world!"
 }
